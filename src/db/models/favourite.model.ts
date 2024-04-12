78377ce3f381a28d9491473236c5db56ff3990e5
Holutunde <Holutunde99@gmail.com>
2024-04-12 22:55:00 +0100

    added favourite model

---
+++ src/db/models/favourite.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, Model } from "mongoose";
+
+// Define the interface for the favorite model
+export interface IFavorite extends Document {
+  userId: mongoose.Types.ObjectId;
+  mediaId?: number;
+  printId?: string;
+}
+
+// Define the schema for the favorite model
+const FavoriteSchema = new mongoose.Schema<IFavorite>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Reference to the user schema
+      required: true,
+    },
+    mediaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "billboardMediaApplication",
+      required: true,
+    },
+    printId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "printMediaApplication", // Reference to the user schema
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const FavoriteModel: Model<IFavorite> = mongoose.model<IFavorite>(
+  "Favorite",
+  FavoriteSchema
+);
+
+export default FavoriteModel;
