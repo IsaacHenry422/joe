e7577ad6515a52479584744ee4e053484647906f
hardeymolar <abdulmalikgbolahan95@gmail.com>
2024-06-03 14:18:22 +0200

    for mediaType

--- src/api/v1/controllers/billboardMediaApplication.controller.ts
+++ src/api/v1/controllers/billboardMediaApplication.controller.ts
@@ -204,6 +204,7 @@ class applicationMediaController {
   async updateMediaApplication(req: Request, res: Response) {
     const { productId } = req.params;
     const {
+      mediaType,
       status,
       listingTitle,
       description,
@@ -225,6 +226,7 @@ class applicationMediaController {
         "MISSING_REQUIRED_FIELD"
       );
     if (
+      !mediaType &&
       !status &&
       !listingTitle &&
       !description &&
@@ -246,6 +248,7 @@ class applicationMediaController {
       );
     }
     const updateFields = {
+      mediaType,
       status,
       listingTitle,
       description,
