315ef5586cb5d742c5fe3fe5a13e8fe3967197a8
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2024-09-27 19:47:44 +0100

    Update index.ts

--- src/api/v1/controllers/index.ts
+++ src/api/v1/controllers/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 class IndexController {
   welcomeHandler(req: Request, res: Response) {
     const payload = {
-      message: "Welcome to the root of the secured VAAD API",
+      message: "Welcome to VAAD API",
     };
 
     res.ok(payload);
